3ffe7188c9ece4e9debc39839bf864a3114ea23b
Dinar Khalfin <khdi1011@hs-karlsruhe.de>
2017-06-23 16:51:21 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,18 @@ public class Calculate{
 	
 	public static void main(String[] arg){
 		System.out.println("Calculate...");
+	int first = Integer.valueOf(arg[0]);
+	int second = Integer.valueOf(arg[1]);
+	int summ = first + second;
+	int diff = first - second;
+	int mult = first * second;
+	int div = first/second;
+	int exp = Math.pow(first, second);
+	System.out.println("Summe " + summ);
+	System.out.println("Differenz" + diff);
+	System.out.println("Multiplikation " + mult);
+	System.out.println("Division " + div);
+	System.out.println("Potenzierung " + exp);	
 		
 	}
 }
